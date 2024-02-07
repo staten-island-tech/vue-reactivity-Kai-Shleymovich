429b0f03c9bc518849ff48c98ce686b005fcc893
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-07 13:18:53 -0500

    started

---
+++ vite-project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+    <h1>Which Song from "Montagne of HECK" are you?</h1>
+  </div>
+  
+</body>
+</html>
+</template>
+
+<style>
+h1{
+  font-size: 3rem;
+}
+</style>
