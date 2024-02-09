0b74b81cc4baa8c1db3126b30b3c984ab87b87aa
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-09 13:20:57 -0500

    html done

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -12,9 +12,35 @@ import HelloWorld from './components/HelloWorld.vue'
 </head>
 <body>
   <div>
-    <h1>Which Song from "Montagne of HECK" are you?</h1>
+    <h1>Which Song from "Montage of HECK" are you?</h1>
   </div>
+  <div class="quiz">
   
+  <h2 class="quiz-question">Q1: Do You Like spoken word songs?</h2>
+  <ul data-quiz-question="1">
+    <input type='checkbox' class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  
+  <h2 class="quiz-question">Q2: Do you like Nirvana's music style?</h2>
+  <ul data-quiz-question="2">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  <h2 class="quiz-question">Q3: Are You Sad?</h2>
+  <ul data-quiz-question="3">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  <h2 class="quiz-question">Q4: Are You a Fan of Yodeling?</h2>
+  <ul data-quiz-question="4">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+</div>
 </body>
 </html>
 </template>
