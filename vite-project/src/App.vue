12d40a7b652957f1c3d2ddd0ab4a2ef0a3e085a8
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-12 13:19:27 -0500

    pohgvc

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -1,6 +1,8 @@
+
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/questions.vue'
+import questions from './components/questions.vue';
 </script>
 
 <template>
@@ -12,35 +14,10 @@ import HelloWorld from './components/HelloWorld.vue'
 </head>
 <body>
   <div>
-    <h1>Which Song from "Montage of HECK" are you?</h1>
-  </div>
-  <div class="quiz">
-  
-  <h2 class="quiz-question">Q1: Do You Like spoken word songs?</h2>
-  <ul data-quiz-question="1">
-    <input type='checkbox' class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
-  </ul>
-  
-  
-  <h2 class="quiz-question">Q2: Do you like Nirvana's music style?</h2>
-  <ul data-quiz-question="2">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
-  </ul>
-  
-  <h2 class="quiz-question">Q3: Are You Sad?</h2>
-  <ul data-quiz-question="3">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
-  </ul>
-  
-  <h2 class="quiz-question">Q4: Are You a Fan of Yodeling?</h2>
-  <ul data-quiz-question="4">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
-  </ul>
-</div>
+    <form action="submit">
+    <questions />
+  </form>
+    </div>
 </body>
 </html>
 </template>
---
+++ vite-project/src/components/questions.vue
@@ -0,0 +1,50 @@
+<template>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+  </head>
+  <body>
+    <div>
+      <h1>Which Song from "Montage of HECK" are you?</h1>
+  </div>
+  <div class="quiz">
+  
+  <h2 class="quiz-question">Q1: Do You Like spoken word songs?</h2>
+  <ul data-quiz-question="1">
+    <input type='radio' class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  
+  <h2 class="quiz-question">Q2: Do you like Nirvana's music style?</h2>
+  <ul data-quiz-question="2">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  <h2 class="quiz-question">Q3: Are You Sad?</h2>
+  <ul data-quiz-question="3">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+  
+  <h2 class="quiz-question">Q4: Are You a Fan of Yodeling?</h2>
+  <ul data-quiz-question="4">
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
+    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+  </ul>
+</div>
+    
+  </body>
+  </html>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
