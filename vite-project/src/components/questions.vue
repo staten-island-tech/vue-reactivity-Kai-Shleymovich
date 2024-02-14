0f92fc1026a8ae8105fd01cb0d40f56f25e4808b
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-14 13:19:45 -0500

    buttons

--- vite-project/src/components/questions.vue
+++ vite-project/src/components/questions.vue
@@ -9,33 +9,33 @@
     <div>
       <h1>Which Song from "Montage of HECK" are you?</h1>
   </div>
-  <div class="quiz">
+  <form class="quiz">
   
   <h2 class="quiz-question">Q1: Do You Like spoken word songs?</h2>
   <ul data-quiz-question="1">
-    <input type='radio' class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="radio" class="quiz-answer" data-quiz-answer="N">NO</input>
+    <input type='radio' class="quiz-answer" data-quiz-answer="Y" name="question 1">YES</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="N" name="question 1">NO</input>
   </ul>
   
   
   <h2 class="quiz-question">Q2: Do you like Nirvana's music style?</h2>
   <ul data-quiz-question="2">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="Y" name="question 2">YES</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="N" name="question 2">NO</input>
   </ul>
   
   <h2 class="quiz-question">Q3: Are You Sad?</h2>
   <ul data-quiz-question="3">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="Y" name="question 3">YES</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="N" name="question 3">NO</input>
   </ul>
   
   <h2 class="quiz-question">Q4: Are You a Fan of Yodeling?</h2>
   <ul data-quiz-question="4">
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="Y">YES</input>
-    <input type="checkbox" class="quiz-answer" data-quiz-answer="N">NO</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="Y" name="question 4">YES</input>
+    <input type="radio" class="quiz-answer" data-quiz-answer="N" name="question 4">NO</input>
   </ul>
-</div>
+</form>
     
   </body>
   </html>
