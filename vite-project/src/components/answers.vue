1d155c8e339911ffebbfc7501ef3d4984b3c9e31
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-15 13:36:40 -0500

    started array

---
+++ vite-project/src/components/answers.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+ const answers = [
+  {
+    songName: 'The Yodel Song'
+  },
+  {
+    songName: 'Aberdeen'
+  },
+  {
+    songName: '19888 Capitol Lake Jam Commercial'
+  },
+  {
+    songName: 'Burn The Rain'
+  }
+  {
+    songName: 'Do Re Mi - Meledy'
+  }
+ ]
+</script>
+
+<style lang="scss" scoped>
+
+</style>
