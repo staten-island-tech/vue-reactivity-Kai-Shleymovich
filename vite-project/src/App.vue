d48a66d3b6e718a651513a109d7ff54ef0e214b6
Kai-Shleymovich <124058665+Kai-Shleymovich@users.noreply.github.com>
2024-02-28 13:19:31 -0500

    dwfrtgyhtrertyhjytrghrr

--- vite-project/src/App.vue
+++ vite-project/src/App.vue
@@ -3,6 +3,57 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/questions.vue'
 import questions from './components/questions.vue';
+const answers = [
+  {
+    songName: 'The Yodel Song'
+  },
+  {
+    songName: 'Aberdeen'
+  },
+  {
+    songName: '19888 Capitol Lake Jam Commercial'
+  },
+  {
+    songName: 'Burn The Rain'
+  },
+  {
+    songName: 'Do Re Mi - Meledy'
+  },
+  {
+    songName: 'Scream'
+  },
+  {
+    songName: 'Sappy - Early Demo'
+  },
+  {
+    songName: 'And I Love Her'
+  },
+  {
+    songName: 'Desire'
+  },
+  {
+    songName: 'Letters To Frances'
+  },
+  {
+    songName: 'Underground Celebritism'
+  },
+  {
+    songName: "Change Me / Burn My Britches / Something In The Way - Early Demo"
+  },
+  {
+    songName: 'Reverb Expierement'
+  },
+  {
+    songName: 'Beans'
+  },
+  {
+    songName: 'The Happy Guitar'
+  },
+  {
+    songName: "Been A Son - Early Demo"
+  },
+ ]
+ console.log(answers)
 </script>
 
 <template>
--- vite-project/src/components/answers.vue
+++ vite-project/src/components/answers.vue
@@ -5,56 +5,7 @@
 </template>
 
 <script setup>
- const answers = [
-  {
-    songName: 'The Yodel Song'
-  },
-  {
-    songName: 'Aberdeen'
-  },
-  {
-    songName: '19888 Capitol Lake Jam Commercial'
-  },
-  {
-    songName: 'Burn The Rain'
-  },
-  {
-    songName: 'Do Re Mi - Meledy'
-  },
-  {
-    songName: 'Scream'
-  },
-  {
-    songName: 'Sappy - Early Demo'
-  },
-  {
-    songName: 'And I Love Her'
-  },
-  {
-    songName: 'Desire'
-  },
-  {
-    songName: 'Letters To Frances'
-  },
-  {
-    songName: 'Underground Celebritism'
-  },
-  {
-    songName: "Change Me / Burn My Britches / Something In The Way - Early Demo"
-  },
-  {
-    songName: 'Reverb Expierement'
-  },
-  {
-    songName: 'Beans'
-  },
-  {
-    songName: 'The Happy Guitar'
-  },
-  {
-    songName: "Been A Son - Early Demo"
-  },
- ]
+
 console.log("test")
 </script>
 
